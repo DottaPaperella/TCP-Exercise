6ce5da0c4742d6df4cdf8f46b62c6c290d853dd0
djsdvg <leonardo.ballini@protonmail.com>
2017-10-25 19:26:14 +0200

    1.0

---
+++ tcp/MainTCP.java
@@ -0,0 +1,27 @@
+
+package tcp;
+
+/**
+ *
+ * @author djsdvg
+ */
+public class MainTCP {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+/* EXAMPLE
+        TCP tcp1 = new TCP(104000,1200, 24000,20);
+        TCP tcp2 = new TCP(46500,1500, 24000);
+        TCP tcp3 = new TCP(96000, 1000,32000);
+        int down3[]={6,7,14,15};
+        int down1[]={7,8,13,14,15,16,17,18,19,20};
+        int down2[]={3,4,5,6,7,14,15};
+        tcp1.draw(down1);
+        tcp2.draw(down2);
+        tcp3.draw(down3);
+        System.out.println(tcp3);
+        System.out.println(tcp2);
+        System.out.println(tcp1);
+*/
+    }
+}
---
+++ tcp/TCP.java
@@ -0,0 +1,132 @@
+
+package tcp;
+
+/**
+ *
+ * @author djsdvg
+ */
+public class TCP {
+    int numSegments;
+    int byt;
+    int mSS;
+    int cWND;
+    int sSTHRESH;
+    int []grafico;
+    int totSeg;
+    int rCVWND;
+    
+    
+    /**
+     * 
+     * @param byt byte that must be send through TCP
+     * @param mSS segments' size
+     * @param rCVWND windows' max size
+     */
+    public TCP( int byt, int mSS, int rCVWND) {
+        this.numSegments = byt/mSS;
+        this.byt = byt;
+        this.mSS = mSS;
+        this.cWND = 1;
+        this.rCVWND=rCVWND/mSS;
+        this.sSTHRESH = rCVWND/(2*mSS);
+        grafico=new int[100];
+        totSeg=0;
+    }
+    
+    /**
+     * 
+     * @param byt byte that must be send through TCP
+     * @param mSS segments' size
+     * @param rCVWND windows' max size
+     * @param sSTHRESH  SSTHRESH size
+     */
+    public TCP( int byt, int mSS, int rCVWND,int sSTHRESH) {
+        
+        this.numSegments = byt/mSS;
+        if(byt%mSS!=0)
+            this.numSegments ++;    
+        this.byt = byt;
+        this.mSS = mSS;
+        this.cWND = 1;
+        this.rCVWND=rCVWND/mSS;
+        this.sSTHRESH = sSTHRESH;
+        grafico=new int[100];
+        totSeg=0;
+    }
+    
+    /**
+     * fill an array with the solution of the exercise
+     */
+    public void draw(){
+        int posDown=0;
+        int posGraf=0;
+        
+        while(totSeg<numSegments){
+            
+                totSeg+=cWND;
+                grafico[posGraf]=cWND;
+                posGraf++;
+                if(cWND<sSTHRESH)
+                    cWND=Integer.min(cWND*2,sSTHRESH);
+                else
+                    cWND=Integer.min(cWND+1,rCVWND);
+            
+            
+            
+        }
+        
+    }
+    
+    
+    /**
+     * fill an array with the solution of the exercise
+     * @param down array that contain the time when the network is offline
+     */
+    public void draw(int down[]){
+        int posDown=0;
+        int posGraf=0;
+        int failCons=0;
+        
+        while(totSeg<numSegments){
+            if(down[posDown]!=posGraf){
+                failCons=0;
+                totSeg+=cWND;
+                grafico[posGraf]=cWND;
+                posGraf++;
+                if(cWND<sSTHRESH)
+                    cWND=Integer.min(cWND*2,sSTHRESH);
+                else
+                    cWND=Integer.min(cWND+1,rCVWND);
+            }
+            else{
+                failCons++;
+                for(int k=0;k<(failCons*2);k++){
+                    grafico[posGraf]=cWND-2*cWND;
+                    posGraf++;  
+                    if(down[posDown]<=posGraf)
+                        posDown=Integer.min(posDown+1,down.length-1);
+
+                }
+                sSTHRESH=Integer.max(cWND/2,2);
+                cWND=1;
+            }
+            
+        }
+    }
+    
+    @Override
+    /**
+     * Print the array with the solution of the exercise
+     */
+    public String toString(){
+        String toReturn="";
+        for(int i=0;grafico[i]!=0;i++){
+            toReturn+=grafico[i]+" ";
+        }
+        return toReturn;
+    }
+    
+    
+    
+    
+}
